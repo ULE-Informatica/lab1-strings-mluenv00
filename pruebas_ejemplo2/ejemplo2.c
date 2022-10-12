236535d0940ffcbe5ba6b4e8ff3d36499ea059e0
Marcos Luengo Viñuela <mluenv00@estudiantes.unileon.es>
2022-10-12 17:34:35 +0200

    Ejemplo 2 - Modificacion código c arreglando leaks arrojados por ASAN

--- pruebas_ejemplo2/ejemplo2.c
+++ pruebas_ejemplo2/ejemplo2.c
@@ -4,31 +4,111 @@
 #include <errno.h>
 #include <limits.h>
 
+// This is the correct function without memory leaks problems
+// Only extra comprobations are added
 void optionOne(){
-    char *s = (char *) malloc(100);
-    strcpy(s, "Welcome Sanitizer!");
-    printf("string is: %s\n", s);
-    free(s);
+
+    // It`s neccesary to ensure that we reserve the correct memory
+    char exampleString[]="Welcome Sanitizer!";
+    int memoryNeeded=sizeof(exampleString);
+
+    long int reservedMemory=100*sizeof(char); //tam of reserved memory (100 chars)
+    char *s = (char *) malloc(reservedMemory);
+
+    if (reservedMemory >= memoryNeeded){
+        strcpy(s, "Welcome Sanitizer!");
+        printf("string is: %s\n", s);
+        free(s); // the memory is liberated after the last use.
+    }
+    else
+    {
+        fprintf(stderr, "%s\n", "Error => Not enought reserved memory");
+        free(s); // the memory is liberated after the last use.
+        exit(-1);
+    }
+
+    
 }
 
+// This is the function with memory leaks problems
 void optionTwo(){
-    char *s = (char *) malloc(100);
-    strcpy(s, "Welcome Sanitizer!");
-    printf("string is: %s\n", s);
+
+    // It`s neccesary to ensure that we reserve the correct memory
+    char exampleString[]="Welcome Sanitizer!";
+    int memoryNeeded=sizeof(exampleString);
+
+    long int reservedMemory=100*sizeof(char); //tam of reserved memory (100 chars)
+    char *s = (char *) malloc(reservedMemory);
+
+    if (reservedMemory >= memoryNeeded){
+        strcpy(s, "Welcome Sanitizer!");
+        printf("string is: %s\n", s);
+        free(s); // the memory is liberated after the last use.
+    }
+    else
+    {
+        fprintf(stderr, "%s\n", "Error => Not enought reserved memory");
+        free(s); // the memory is liberated after the last use.
+        exit(-1);
+    }
+
 }
 
+// This is the function with heap use after free
 void optionThree(){
-    char *s = (char *) malloc(100);
-    free(s);
-    strcpy(s, "Welcome Sanitizer!");
-    printf("string is: %s\n", s);
+    // When we use malloc, we will request a block of memory from the heap. 
+    //char *s = (char *) malloc(100);
+    //free(s); // the memory is liberated after the memory reservation
+    
+    // Then, try to modify and read the heap
+    //strcpy(s, "Welcome Sanitizer!");
+    //printf("string is: %s\n", s);
+
+    // It`s neccesary to ensure that we reserve the correct memory
+    char exampleString[]="Welcome Sanitizer!";
+    int memoryNeeded=sizeof(exampleString);
+
+    long int reservedMemory=100*sizeof(char); //tam of reserved memory (100 chars)
+    char *s = (char *) malloc(reservedMemory);
+
+    if (reservedMemory >= memoryNeeded){
+        strcpy(s, "Welcome Sanitizer!");
+        printf("string is: %s\n", s);
+        free(s); // the memory is liberated after the last use.
+    }
+    else
+    {
+        fprintf(stderr, "%s\n", "Error => Not enought reserved memory");
+        free(s); // the memory is liberated after the last use.
+        exit(-1);
+    }
 }
 
-void optionFour(){
-    char *s = (char *) malloc(18);
-    strcpy(s,  "Welcome Sanitizer!");
-    printf("string is: %s\n", s);
-    free(s);
+// This is the function with memory reservation problems that produces a heap overflow
+void optionFour()
+{   
+    //char *s = (char *) malloc(18); // we need 19 instead 18 because the '\0' character
+    
+    // It`s neccesary to ensure that we reserve the correct memory
+    char exampleString[]="Welcome Sanitizer!";
+    int memoryNeeded=sizeof(exampleString);
+
+    //long int reservedMemory=18*sizeof(char); //Incorrect tam of reserved memory (18 chars)
+    long int reservedMemory=19*sizeof(char); //Correct tam of reserved memory (19 chars)
+    char *s = (char *) malloc(reservedMemory);
+
+    if (reservedMemory >= memoryNeeded){
+        strcpy(s, "Welcome Sanitizer!");
+        printf("string is: %s\n", s);
+        free(s); // the memory is liberated after the last use.
+    }
+    else
+    {
+        fprintf(stderr, "%s\n", "Error => Not enought reserved memory");
+        free(s); // the memory is liberated after the last use.
+        exit(-1);
+    }
+    
 }
 
 int main(int argc, const char *argv[]) {
